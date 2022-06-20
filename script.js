e199017b418df85fd5f21bfd90fb1c7a34b42b29
Aneal Laryea <aneallaryea100@gmail.com>
2022-06-20 11:38:11 -0700

    hamburger javascript responding

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-let hambx = document.querySelector('.hambx');
-let divMenb = document.querySelector('.menb');
-hambx.addEventListener('click',function(){
-    let ulMenb =document.createElement('ul');
-    let liMenb = document.createElement('li');
-    let aMenb = document.createElement('a');
-    aMenb.textContent = 'Portfolio';
-    aMenb.textContent = 'About';
-    aMenb.textContent = 'Contact';
-    liMenb.appendChild(aMenb);
-    ulMenb.appendChild(liMenb);
-    divMenb.appendChild(ulMenb);
-    console.log('hamburger');
+// let hambx = document.querySelector('.hambx');
+// let divMenb = document.querySelector('.menb');
+// hambx.addEventListener('click',function(){
+//     let ulMenb =document.createElement('ul');
+//     let liMenb = document.createElement('li');
+//     let aMenb = document.createElement('a');
+//     aMenb.textContent = 'Portfolio';
+//     liMenb.appendChild(aMenb);
+//     aMenb.textContent = 'About';
+//     liMenb.appendChild(aMenb);
+//     aMenb.textContent = 'Contact';
+//     liMenb.appendChild(aMenb);
+//     ulMenb.appendChild(liMenb);
+//     divMenb.appendChild(ulMenb);
+//     console.log('hamburger');
+// });
+
+const hambx = document.querySelector('.hambx');
+const close = document.querySelector('.close');
+const nav = document.querySelector('.navvme');
+const navLink = document.querySelectorAll('.navlinks');
+hambx.addEventListener('click', () => {
+  nav.classList.add('active');
 });
+
+close.addEventListener('click', () => {
+  nav.classList.remove('active');
+});
+
+navLink.forEach((element) => element.addEventListener('click', () => {
+  nav.classList.remove('active');
+}));
